507c751fc25b62c4ff47df74d36b0e6bf2c6a7cf
Jessie Callaway <jessie.callaway91@gmail.com>
2022-08-01 21:39:14 -0500

    commented lots of test code

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
-let catchPhrase = "I'm a cool mom!";
+// let catchPhrase = "I'm a cool mom!";
+// console.log(catchPhrase);
 
-console.log(catchPhrase);
+// !LEARNING if STATEMENTS!
+// const age = 25;
+// if (age > 20) {
+//	console.log('delete it, old!');
+// }
+
+// const ninjas = ['guy', 'girl', 'they', 'bitch'];
+// if (ninjas.length > 3) {
+//	console.log("that's a lot of ninjas");
+// }
+
+// !LEARNING else if STATEMENTS and LOGICAL AND && OR || THINGS!
+
+// const password = 'p@ss';
+// if (password.length >= 12 && password.includes('@')) {
+//	console.log('that is so good!');
+// } else if (
+//	password.length >= 8 ||
+//	(password.includes('@') && password.length > 5)
+// ) {
+// 	console.log('it is okay');
+// } else {
+// 	console.log('nope');
+// }
+
+// logical NOT (!)
+// if(!false) means true
+//
