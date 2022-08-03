57a2d34e246fd8637ad25048c0a75a2f01813a3a
Jessie Callaway <jessie.callaway91@gmail.com>
2022-08-02 19:44:53 -0500

    added README and some test js code

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // let catchPhrase = "I'm a cool mom!";
 // console.log(catchPhrase);
 
+//--------------------------------------------------------
 // !LEARNING if STATEMENTS!
 // const age = 25;
 // if (age > 20) {
 //	console.log('delete it, old!');
 // }
-
 // const ninjas = ['guy', 'girl', 'they', 'bitch'];
 // if (ninjas.length > 3) {
 //	console.log("that's a lot of ninjas");
 // }
 
+//--------------------------------------------------------
 // !LEARNING else if STATEMENTS and LOGICAL AND && OR || THINGS!
-
 // const password = 'p@ss';
 // if (password.length >= 12 && password.includes('@')) {
 //	console.log('that is so good!');
@@ -25,7 +25,59 @@
 // } else {
 // 	console.log('nope');
 // }
-
 // logical NOT (!)
 // if(!false) means true
-//
+
+//--------------------------------------------------------
+// !break STOPS THE LOOP AND continue SKIPS THAT INSTANCE!
+// const scores = [50, 25, 0, 30, 100, 20, 10];
+// for (let i = 0; i < scores.length; i++) {
+//	if (scores[i] === 0) {
+//		continue;
+//	}
+//	console.log('your score: ', scores[i]);
+//	if (scores[i] === 100) {
+//		console.log('congrats!');
+//		break;
+//	}
+// }
+
+//--------------------------------------------------------
+// !USING A switch STATEMENT TO CHECK LOTS OF CONDITIONS INSTEAD OF else if!
+// const grade = 'G';
+// switch (grade) {
+//	case 'A':
+//		console.log('you got an A!');
+//		break;
+//	case 'B':
+//		console.log('you got an B');
+//		break;
+//	case 'C':
+//		console.log('you got an C');
+//		break;
+//	case 'D':
+//		console.log('you got an D...');
+//		break;
+//	case 'E':
+//		console.log('you got an E..?');
+//		break;
+//	case 'F':
+//		console.log('you got an EEEFFFFF!');
+//		break;
+//	default:
+//		console.log('dot a grade, girl');
+// }
+
+//--------------------------------------------------------
+// let age = 30; //route/global scope
+// if (true) {
+//	let age = 40; // nested code block
+//	let name = 'shawn';
+//	console.log('inside code block: ', age, name); // grabs nested age/name
+//	if (true);
+//	{
+//		let age = 50;
+//		console.log('in 2nd code block: ', age); // even further nested
+//	}
+// }
+// console.log('outside code block: ', age, name); // grabs global age/name
